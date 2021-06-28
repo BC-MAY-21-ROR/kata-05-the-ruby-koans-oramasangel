6e2aab093ce928453f4f745750dea63ba5d58646
Angel Arturo Oramas Rojas <oramasrojas199angelarturo@hotmail.com>
2021-06-28 14:02:21 -0500

    Koans, 207 completed

--- src/triangle.rb
+++ src/triangle.rb
@@ -15,6 +15,23 @@
 #
 def triangle(a, b, c)
   # WRITE THIS CODE
+  if [a,b,c].any? {|x| x <= 0}
+    raise TriangleError, "Sides must have positive length" 
+  end
+
+  sides = [a,b,c].sort
+
+  unless sides[0]+sides[1] > sides[2]
+    raise TriangleError, "Does not satisfy triangle inequality"
+  end
+
+  if a == b and b == c
+    :equilateral
+  elsif a == b or b == c or a == c
+    :isosceles
+  else
+    :scalene
+  end
 end
 
 # Error class used in part 2.  No need to change this code.
